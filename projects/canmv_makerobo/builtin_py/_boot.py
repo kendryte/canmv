8426c4d455dbe2aa96ff0207c194924f2d44d23c
kendryte747 <kendryte747@gmail.com>
2023-02-15 16:19:25 +0800

    canmv_makerobo: update _boot.py

--- projects/canmv_makerobo/builtin_py/_boot.py
+++ projects/canmv_makerobo/builtin_py/_boot.py
@@ -46,13 +46,12 @@ try:
     lcd.init()
     loading = image.Image(size=(lcd.width(), lcd.height()))
     loading.draw_rectangle((0, 0, lcd.width(), lcd.height()), fill=True, color=(0, 81, 137))
-    info = "Welcome to CanMV"
+    info = "Welcome to Makerobo CanMV"
     loading.draw_string(int(lcd.width()//2 - len(info) * 5), (lcd.height())//4, info, color=(255, 255, 255), scale=2, mono_space=0)
-    v = sys.implementation.version
-    vers = 'V{}.{}.{} : canaan-creative.com'.format(v[0],v[1],v[2])
-    loading.draw_string(int(lcd.width()//2 - len(info) * 6), (lcd.height())//3 + 20, vers, color=(255, 255, 255), scale=1, mono_space=1)
+    vers = 'V2.0 : https://www.loborobot.com'
+    loading.draw_string(int(lcd.width()//2 - len(info) * 5), (lcd.height())//3 + 20, vers, color=(255, 255, 255), scale=1, mono_space=1)
     lcd.display(loading)
-    del loading, v, info, vers
+    del loading, info, vers
     gc.collect()
 finally:
     gc.collect()
